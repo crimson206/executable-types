c253937b5c4570370a8373a534e5c3b54636c928
Sisung Kim <sisung.kim1@gmail.com>
2024-10-07 15:26:30 +0200

    setup

--- beta/scripts/setup_env.sh
+++ beta/scripts/setup_env.sh
@@ -2,9 +2,9 @@
 
 read -p "Please enter the Python version you want to use (e.g., 3.9): " PYTHON_VERSION
 
-conda create --name test-package-beta python=$PYTHON_VERSION -y
+conda create --name executable-types-beta python=$PYTHON_VERSION -y
 
-conda activate test-package-beta
+conda activate executable-types-beta
 
 pip install -r requirements.txt
 pip install -r requirements_test.txt
--- stable/scripts/setup_env.sh
+++ stable/scripts/setup_env.sh
@@ -2,9 +2,9 @@
 
 read -p "Please enter the Python version you want to use (e.g., 3.9): " PYTHON_VERSION
 
-conda create --name test-package python=$PYTHON_VERSION -y
+conda create --name executable-types python=$PYTHON_VERSION -y
 
-conda activate test-package
+conda activate executable-types
 
 pip install -r requirements.txt
 pip install -r requirements_test.txt
